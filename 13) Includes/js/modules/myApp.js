1c7c77e61568ff493950ba26e5d52fbf35d9d48b
Jerry Thimothy J <j.jerrythimothy@tcs.com>
2016-09-03 22:18:31 +0530

    New Programs

---
+++ 13) Includes/js/modules/myApp.js
@@ -0,0 +1,8 @@
+(function(angular) {
+	'use strict';
+
+	// Always follow IIFE method to avoid conflicts and strict mode to stop execution when error
+	
+	// Follow the below line to define an angular application; The packages included within [] are the dependencies
+	angular.module('myApp', ['ui.bootstrap']);
+})(window.angular);
